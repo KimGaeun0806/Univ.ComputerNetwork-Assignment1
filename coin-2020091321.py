e9196654c0514332f2b107641a1c720d0f4696c3
KimGaeun0806 <80266418+KimGaeun0806@users.noreply.github.com>
2022-12-02 02:41:42 +0900

    Update coin-2020091321.py

--- coin-2020091321.py
+++ coin-2020091321.py
@@ -1,82 +1,64 @@
-# import requests module
-import sched
 import requests
-
-# run BackgroundScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
-
-import telepot
-
+from apscheduler.schedulers.background import BlockingScheduler
+from telegram.ext import Updater, CommandHandler
 import telegram
-
-from pprint import pprint
-
-from telepot.loop import MessageLoop
-
-# get a webpage
-# r -> Response object
-r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR&extraParams=Bitfinex')
-
-# read the content of the server's response
-# use json to access response objects
-response = r.json()
-eur_price = response['EUR']
-usd_price = response['USD']
-# a = r.text
-# print(a['EUR'])
-# eur_price = r.text.EUR
-# usd_price = r.text.USD
+from datetime import datetime
 
 # use BackgroundScheduler
 scheduler = BackgroundScheduler()
-scheduler.start()
-
-# account test 
-# bot = telepot.Bot('5561261260:AAGjGQ8arLymg-OzLaiDcJArjh1JpAOjQwo')
-# bot.getMe()
-
-# getUpdates() returns an array of Update objects
-# offset=100000001 -> to avoid getting the same old messages 
-# response = bot.getUpdates(offset=100000001)
-# print(response)
-
-# to monitor the messages
-# def handle(msg):
-#     pprint(msg)
-
-# MessageLoop(bot, handle).run_as_thread()
-
-# bot.sendMessage(5561261260, 'Hey!')
-
-
 
 api_key = "5561261260:AAGjGQ8arLymg-OzLaiDcJArjh1JpAOjQwo"
 id = "5487316245"
- 
+
+# use telegram to access the bot account
 bot = telegram.Bot(token=api_key)
+
+# open the coinPrice.csv file in write format and save it to f
+f = open("coinPrice.csv", "w")
+
+# make file header
+f.write("Date, BTC-USD, BTC-EUR\n")
 
 # functions that send texts
 def send_coin_price():
-    bot.sendMessage(chat_id=id, text='text')
-    # f"BTC-USD: {usd_price}\nBTC-EUR: {eur_price}"
+    # get a webpage
+    # r -> Response object
+    r = requests.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR')
 
+    # read the content of the server's response
+    # use json to access response objects
+    response = r.json()
+    eur_price = response['EUR']
+    usd_price = response['USD']
+
+    # send messages
+    bot.sendMessage(chat_id=id, text=f"BTC-USD: {usd_price}\nBTC-EUR: {eur_price}")
+
+    # store data in csv file 
+    f.write(str(datetime.now()) + ',' + str(usd_price) + ',' + str(eur_price) + '\n')
+
+    # function that works when the stop command is executed
+    def stop(update, context):
+        scheduler.remove_job('my_job_id')
+        f.close()
+        
+    updater = Updater(token=api_key, use_context=True)
+
+    # Run the stop function when /stop is entered
+    stop_handler = CommandHandler('stop', stop)
+    updater.dispatcher.add_handler(stop_handler)
+
+    # start updater
+    updater.start_polling()
+    updater.idle()
+
+# set timezone to remove warning
+scheduler = BlockingScheduler(timezone='Asia/Seoul')
 
 # calling add_job() -> add jobs to a scheduler
-# interval -> use when I want to run the job at fixed intervals of time 
-scheduler.add_job(send_coin_price, 'interval', seconds=1)
-#  id='my_job_id'
-# scheduler.remove_job('my_job_id')
-# scheduler.add_job(send_coin_price, 'cron', second='1')
+# cron -> use when run the job periodically at certain times of day
+scheduler.add_job(send_coin_price, 'cron', second='1', id='my_job_id')
 
-# scheduler.start(paused=True)
-
-
-# file:///C:/Users/SAMSUNG/Desktop/3-1/%EC%BB%B4%ED%93%A8%ED%84%B0%EB%84%A4%ED%8A%B8%EC%9B%8C%ED%81%AC/ene419-assignment-1-2022.pdf
-# https://min-api.cryptocompare.com/documentation?key=Price&cat=SingleSymbolPriceEndpoint
-# https://requests.readthedocs.io/en/latest/user/quickstart/
-# https://apscheduler.readthedocs.io/en/latest/userguide.html
-# https://telepot.readthedocs.io/en/latest/
-
-# https://ddolcat.tistory.com/746
-# https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=varkiry05&logNo=221257249284
-# 
+# start the scheduler
+scheduler.start()
